b0aa2a25a27e94b0413c7f8859d3de858d44199d
MohammadaliFakhari <alifakhary622@gmail.com.com>
2024-11-05 17:19:47 +0330

    feat: create start menu and loading screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loaderUI;
+    public Slider progressBar;
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadSceneCoroutine(index));
+    }
+
+    public IEnumerator LoadSceneCoroutine(int index)
+    {
+        progressBar.value = 0;
+        loaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+
+        float loadingProgress = 0;
+
+        while (!asyncOperation.isDone)
+        {
+            loadingProgress = Mathf.MoveTowards(loadingProgress, asyncOperation.progress + 0.01f, Time.deltaTime);
+            progressBar.value = loadingProgress;
+
+            if (loadingProgress >= 0.9f)
+            {
+                progressBar.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+    
+}
